7146a471932aa9cfdc113f885c32876259bec7c9
mhusseinalmed <mohammedhussein19949@gmail.com>
2022-10-24 07:37:34 -0700

    task four

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - a function the print a string
+ * str@: a pointer the point to a variable to be updated
+ *
+ * return nothing
+ */
+
+void _puts(char *str)
+{
+	int length = 0;
+	while(str[length])
+	{
+		_putchar(str[length]);
+		length++;
+	}
+	_putchar('\n');
+}
