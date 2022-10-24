0b197f0f1df9900731a1ebddc563b55fb8573aab
mhusseinalmed <mohammedhussein19949@gmail.com>
2022-10-24 02:37:28 -0700

    first task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * reset_to_98 - reset n value to 98
- * 
- *
+ *n@ is a pointer to the address of the variable c in main function  
+ * no return value
  */
 
 void reset_to_98(int *n)
