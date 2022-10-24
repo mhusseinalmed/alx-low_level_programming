e5b2e4e47c277100d63d0134775adf1788615a1e
mhusseinalmed <mohammedhussein19949@gmail.com>
2022-10-24 07:03:58 -0700

    fix task 0 error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * reset_to_98 - reset n value to 98
- *n@ is a pointer to the address of the variable c in main function  
- * no return value
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: nothing
  */
-
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
-
